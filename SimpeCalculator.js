93d0d5b149972500953d2a68cc86f8a78c42189e
rahmadyan95 <haekal.rahmadyan95@gmail.com>
2025-08-24 18:36:06 +0700

    entahlah

---
+++ SimpeCalculator.js
@@ -0,0 +1,32 @@
+
+class Calculator{
+    constructor() {
+        console.log("Calculator initialized...");
+    }
+
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Tidak bisa membagi dengan nol!");
+        }
+        return a / b;
+    }
+
+}
+
+module.exports = {Calculator}
+
+const calc = new Calculator(); // <- harus pakai ()
+console.log(calc.add(10, 5));  // output: 15
+console.log(calc.divide(20, 4)); // output: 5
